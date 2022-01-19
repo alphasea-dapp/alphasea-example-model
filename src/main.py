b133a3050dccbb310e28b7d3de4b718b0519647a
a <a@example.com>
2022-01-20 02:28:06 +0900

    change schedule

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import time
 import schedule
 from .predict import predict_job
 
-schedule.every().day.at('00:05').do(predict_job)
+schedule.every().day.at('00:04').do(predict_job)
 
 while True:
     schedule.run_pending()
